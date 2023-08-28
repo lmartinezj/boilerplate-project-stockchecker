a588c5fec89ff91f3b8c314067f24ac99cb4bbb3
lmartinez <lmartinezj@gmail.com>
2023-08-28 09:54:37 -0500

    test response

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,8 @@
 'use strict';
 
 module.exports = function (app) {
-
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
-    });
-    
+      console.log(res.text())
+    });    
 };
